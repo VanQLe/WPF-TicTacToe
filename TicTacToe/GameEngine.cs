b28f2b44691562bc47973572ebe06a726123fe97
Van Le <van.le@pridetek.com>
2016-06-03 11:44:38 -0700

    First commit

---
+++ TicTacToe/GameEngine.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class GameEngine
+    {
+        public int[,] board;
+
+        public GameEngine()
+        {
+            board = new int[3,3];
+        }
+
+        public bool CheckWinner(int playerX)
+        {
+
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+     
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    if (board[i, j] != playerX)
+                        break;
+                    if ( j == 2 && board[i,2] == playerX)
+                        return true;
+
+                }
+            }
+            if ((board[0, 0] == playerX && board[1, 1] == playerX && board[2, 2] == playerX) || (board[0, 2] == playerX && board[1, 1] == playerX && board[2, 0] == playerX))
+                return true;
+            
+
+            return false;
+        }
+    }
+}
---
+++ TicTacToe/MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TicTacToe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public GameEngine game;
+        public int turns;
+        public bool GameStarted = false;
+        public bool player1turn = true;
+        public int player1;
+       
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Player1SelectedCircle(object sender, RoutedEventArgs e)
+        {
+            Player2XRB.IsChecked = true;
+            Player1CircleRB.IsChecked = true;
+
+
+        }
+        private void Player2SelectedCircle(object sender, RoutedEventArgs e)
+        {
+            Player1XRB.IsChecked = true;
+            Player2CircleRB.IsChecked = true;
+
+        }
+
+        private void Player1SelectedX(object sender, RoutedEventArgs e)
+        {
+            Player1XRB.IsChecked = true;
+            Player2CircleRB.IsChecked = true;
+
+
+        }
+        private void Player2SelectedX(object sender, RoutedEventArgs e)
+        {
+            Player2XRB.IsChecked = true;
+            Player1CircleRB.IsChecked = true;
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            game = new GameEngine();
+            GameStarted = true;
+            MessageBox.Show("Game Started");
+        }
+
+        private void GameBoardSection00_Click(object sender, RoutedEventArgs e)
+        {
+            // ... Create a new BitmapImage.
+            BitmapImage b = new BitmapImage();
+            b.BeginInit();
+            b.UriSource = new Uri("Resources/PlayerO.png");
+            b.EndInit();
+
+            // ... Get Image reference from sender.
+            var image = sender as Image;
+            // ... Assign Source.
+            image.Source = b;
+        
+    }
+
+        private bool checkGameStatus()
+        {
+            //player1Image = new ImageBrush();
+            //player2Image = new ImageBrush();
+            if (!GameStarted)
+                return false;
+            else
+                return true;
+
+            if (Player1CircleRB.IsChecked == true)
+            {
+                player1 = 0;
+              //  player1Image.ImageSource = new BitmapImage(new Uri("Resources/PlayerO.png", UriKind.Relative));
+               // player2Image.ImageSource = new BitmapImage(new Uri("Resources/PlayerX.png", UriKind.Relative));
+            }
+
+            else
+            {
+                player1 = 1;
+               // player1Image.ImageSource = new BitmapImage(new Uri("Resources/PlayerX.png", UriKind.Relative));
+                //player2Image.ImageSource = new BitmapImage(new Uri("Resources/PlayerO.png", UriKind.Relative));
+            }
+               
+
+           
+           
+        }
+    }
+}
